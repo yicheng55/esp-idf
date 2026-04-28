2f308dcfeb1ba11c98c94a96b069fcaf71b2c340
YI-CHENG <yich_liao@mail.davicom.com.tw>
2026-04-28 18:51:02 +0800

    feat: update DM9058 PTP configuration and driver integration

--- components/esp_eth/src/spi/dm9058/esp_eth_mac_dm9058.c
+++ components/esp_eth/src/spi/dm9058/esp_eth_mac_dm9058.c
@@ -1516,7 +1516,9 @@ esp_eth_mac_t *esp_eth_mac_new_dm9058(const eth_dm9058_config_t *DM9058_config, 
     };
     ESP_GOTO_ON_FALSE(esp_eth_ptp_dm9058_init(&emac->ptp, emac, &ptp_ops) == ESP_OK, NULL, err, TAG, "init dm9058 ptp context failed");
     emac->ptp_auto_process = true;
-#ifdef CONFIG_ETH_DM9058_PTP_TWO_STEP_MODE
+#if defined(CONFIG_NETUTILS_PTPD_TWOSTEP_SYNC)
+    emac->ptp_two_step_mode = true;
+#elif defined(CONFIG_NETUTILS_PTPD)
     emac->ptp_two_step_mode = true;
 #else
     emac->ptp_two_step_mode = false;
